bbd2b528abe63ebecc03894b80f2a0a424e137b6
José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>
2022-10-21 13:08:36 +0200

    Remark which tests are mandatory and which are optional in the test module's docstring

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,15 @@
-"""Use this module to write tests for your wrapper."""
+"""Use this module to write tests for your wrapper.
+
+Modify the files in this module to adapt them to your own wrapper. You can
+organize your test files and their contents in any way you wish and use any
+testing framework.
+
+Writing tests that cover the typical use case of your wrapper (see
+`test_usage.py`) and tests that cover calls to all the wrapper API functions
+you use in your wrapper (see `test_wrapper_api.py`) is very strongly
+recommended, and may even be required if the wrapper is to be officially
+endorsed by us.
+
+Optionally, you can also write unit tests (see `test_unit.py`) and integration
+tests (see `test_integration.py`).
+"""
